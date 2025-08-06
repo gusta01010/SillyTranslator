619795c691af24d195dddf17954c75a40801efc2
Gustavo Lima <sonic8783@hotmail.com>
2025-08-05 23:21:09 -0300

    removed unused files

--- src/card_translator/lib/free_translator.py
+++
@@ -1,6 +0,0 @@
-from deep_translator import GoogleTranslator
-
-class GoogleTranslatorService:
-    def translate(self, text, dest):
-        translated_text = GoogleTranslator(source='auto', target=dest).translate(text)
-        return type("Translation", (), {"text": translated_text})()
